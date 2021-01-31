8d067a8ee58035a554887d55534b4cf2cfe9564d
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-01-31 04:57:04 +0300

    unnecessary lines and variables deleted

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,13 +1,11 @@
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import static java.math.BigDecimal.ROUND_FLOOR;
-
 public class CreditPaymentService {
 
     public long calculate(long loan_amount, int years) {
 
-        double interest = 0.008325; // ежемесячная процентая ставка 9.99 / 12 /100
+        BigDecimal interest = new BigDecimal(0.008325); // ежемесячная процентая ставка 9.99 / 12 /100
 
         int mounth_period = 0;
 
@@ -24,14 +22,11 @@ public class CreditPaymentService {
 
         }
 
-        double interest_sum = interest + 1;  // (i+1)
-
-        BigDecimal interest_sum_D = new BigDecimal(interest_sum);
-        BigDecimal int_sum_degree = interest_sum_D.pow(mounth_period); // (1+i)^n
-        BigDecimal interest_D = new BigDecimal(interest);
-        BigDecimal mult_res = interest_D.multiply(int_sum_degree); // i*(1+i)^n
-        BigDecimal subst_res = int_sum_degree.subtract(BigDecimal.valueOf(1));
-        BigDecimal ann_coeff = mult_res.divide(subst_res, 25, RoundingMode.HALF_DOWN);
+        BigDecimal interest_sum = interest.add(BigDecimal.valueOf(1));  // (i+1)
+        BigDecimal int_sum_degree = interest_sum.pow(mounth_period); // (1+i)^n
+        BigDecimal mult_res = interest.multiply(int_sum_degree); // i*(1+i)^n
+        BigDecimal subst_res = int_sum_degree.subtract(BigDecimal.valueOf(1)); // ((1+i)^n-1)
+        BigDecimal ann_coeff = mult_res.divide(subst_res, 25, RoundingMode.HALF_DOWN); // i*(1+i)^n/((1+i)^n-1)
         BigDecimal loan_amount_D = new BigDecimal(loan_amount);
         BigDecimal ann_month_payment = loan_amount_D.multiply(ann_coeff);
 
